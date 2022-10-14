38f4a4a14914fbc25371b13ff31d31f684f08a5b
Paul Imoke <pauleke65@gmail.com>
2022-10-14 20:23:46 +0000

    add `isAFellow` method

--- TryingMethods.java
+++ TryingMethods.java
@@ -12,4 +12,30 @@ public class TryingMethods {
 
     }
 
+    /**
+     * This method has no return type, it takes a name
+     * and prints whether the person is a MEST Teaching fellow.
+     * If it does not find the name, it prints a not found string.
+     * 
+     * 
+     * @param name {@code String} The name to check if it is a teaching fellow
+     */
+    public void isAFellow(String name) {
+        String formattedName = name.trim().toUpperCase();
+
+        if (formattedName.equals("BRIGHT") || formattedName.equals("LUCKY")) {
+            System.out.println(name + " is a Tech Fellow");
+        } else if (formattedName.equals("LADY-O")) {
+            System.out.println(name + " is a Business Fellow");
+        } else if (formattedName.equals("AFI")) {
+            System.out.println(name + " is a Communications Fellow");
+        } else if (formattedName.equals("DAVID")) {
+            System.out.println(name + " is both a Communications and Business fellow");
+        } else {
+            System.out.println("Sorry, I couldn't find your details");
+
+        }
+
+    }
+
 }
