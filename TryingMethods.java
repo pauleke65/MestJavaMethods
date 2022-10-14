8f55df4c2281cefdc0f5723e356a5eb486138557
Paul Imoke <pauleke65@gmail.com>
2022-10-14 19:34:58 +0000

    create `TryingMethods` class

---
+++ TryingMethods.java
@@ -0,0 +1,11 @@
+/**
+ * The {@code TryingMethods} class contains four(4) methods,
+ * and a <b>main</b> method.
+ * These methods were created as an assignment in the MEST Tech class.
+ * It cannot be instantiated.
+ *
+ * @author Paul Imoke
+ */
+public class TryingMethods {
+
+}
